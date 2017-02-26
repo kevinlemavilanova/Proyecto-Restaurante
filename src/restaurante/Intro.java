b1b2de7db1a3a14901503d0d07ece37168f600f7
kevin <kevin@kevin-PC>
2017-02-26 21:46:04 +0100

    Cambio de ventana a internal frame

--- src/restaurante/Intro.java
+++ src/restaurante/Intro.java
@@ -5,20 +5,20 @@
  */
 package restaurante;
 
-import javafx.scene.Scene;
+import java.awt.Component;
+import javax.swing.JDesktopPane;
 
 /**
  *
- * @author christian
+ * @author kevin
  */
 public class Intro extends javax.swing.JFrame {
-    public Intro JFrame;
+
     /**
-     * Creates new form Intro
+     * Creates new form Intro1
      */
     public Intro() {
         initComponents();
-        
     }
 
     /**
@@ -31,18 +31,17 @@ public class Intro extends javax.swing.JFrame {
     private void initComponents() {
 
         jButton1 = new javax.swing.JButton();
-        jLabel5 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jButton1.setFont(new java.awt.Font("MV Boli", 1, 24)); // NOI18N
-        jButton1.setForeground(new java.awt.Color(255, 255, 255));
         jButton1.setText("<< Entrar >>");
         jButton1.setBorderPainted(false);
         jButton1.setContentAreaFilled(false);
@@ -53,20 +52,20 @@ public class Intro extends javax.swing.JFrame {
         });
         getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 300, 690, 290));
 
-        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/aplicacion/Imagenes/TituloHorizontal.png"))); // NOI18N
-        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 230, 690, 60));
-
         jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/aplicacion/Imagenes/TituloHorizontal.png"))); // NOI18N
         getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 90, 640, 60));
+
+        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/aplicacion/Imagenes/TituloHorizontal.png"))); // NOI18N
+        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 230, 690, 60));
 
         jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/aplicacion/Imagenes/FondoVertical.png"))); // NOI18N
         getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(830, 0, 40, 640));
 
-        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/aplicacion/Imagenes/FondoVertical.png"))); // NOI18N
-        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 0, 40, 640));
-
         jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/aplicacion/Imagenes/TituloPrincipal.png"))); // NOI18N
         getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 100, 590, 190));
+
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/aplicacion/Imagenes/FondoVertical.png"))); // NOI18N
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 0, 40, 640));
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/aplicacion/Imagenes/Fondo.png"))); // NOI18N
         getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 901, 639));
@@ -75,9 +74,9 @@ public class Intro extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        
-        
-
+        Principal principal = new Principal();
+        this.add(principal);
+        principal.show();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
@@ -105,6 +104,7 @@ public class Intro extends javax.swing.JFrame {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(Intro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
--- src/restaurante/Restaurante.java
+++ src/restaurante/Restaurante.java
@@ -28,11 +28,10 @@ import javax.swing.WindowConstants;
  */
 public class Restaurante extends Application {
     private JFrame ventanaPrincipal;
-private JDialog ventanaSecundaria;
+    private JDialog ventanaSecundaria;
 
     @Override
     public void start(Stage primaryStage) {
-        
         new Intro().setVisible(true);
     }
 
@@ -40,55 +39,6 @@ private JDialog ventanaSecundaria;
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-		new Restaurante();
-	}
-	
-	public Restaurante()
-	{
-		// Construcción de ventana principal
-		ventanaPrincipal = new JFrame("Ventana principal");
-		JButton boton = new JButton("Abre secundaria");
-		ventanaPrincipal.getContentPane().add(boton);
-		ventanaPrincipal.pack();
-		
-		// Construcción de ventana secundaria
-		ventanaSecundaria = new JDialog(ventanaPrincipal,"Ventana secundaria");
-		JLabel etiqueta = new JLabel("Hola");
-		ventanaSecundaria.getContentPane().add(etiqueta);
-		ventanaSecundaria.pack();
-
-		// Hacer que el botón abra la ventana secundaria y cierre la
-		// principal
-		boton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				ventanaPrincipal.setVisible(false);
-				ventanaSecundaria.setVisible(true);
-			}
-
-                    @Override
-                    public void actionPerformed(java.awt.event.ActionEvent e) {
-                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                    }
-		
-		});
-		
-		// Hacer que al cerrarse la secundaria con la x de arriba a la
-		// derecha, se muestre la primaria
-		ventanaSecundaria.addWindowListener(new WindowAdapter() {
-			public void windowClosing(WindowEvent e) {
-				ventanaPrincipal.setVisible(true);
-				ventanaSecundaria.setVisible(false);
-			}
-		
-			public void windowClosed(WindowEvent e) {
-				ventanaPrincipal.setVisible(true);
-				ventanaSecundaria.setVisible(false);
-			}
-		});
-		
-		// Mostrar la ventana principal
-		ventanaPrincipal.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		ventanaPrincipal.setVisible(true);
-	}
-    
+        launch(args);
+    }
 }
